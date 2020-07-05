d8bf549f0da726f207619f31944bf4047662667e
yxdao <1378260452@qq.com>
2020-07-05 13:24:06 +0800

    析构函数例子

---
+++ dc13.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int res{
+	public :
+		void setij(int a,int b){
+int c;
+a=c;
+b=a;
+c=b;	
+cout<<"a="<<a<<"\tb="<<b<<end;
+return 0;	}
+
+~res(){};
+}
+int main(){
+int a,b;
+cin>>a>>b;
+res(a,b);
+return 0;
+}
+
+
